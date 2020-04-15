c6d8bec2131201ae99a07a17e1403f26d7ab81e2
tanzeying <>
2020-04-15 14:53:02 +0800

    Delete ocr project.Create redis project.

---
+++ redis/src/test/java/org/taric/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taric.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
